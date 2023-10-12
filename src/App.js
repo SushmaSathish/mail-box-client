97e8de081af0e9b73339f6c3023c87a108ce5735
SushmaSathish <sushmasathish.ngl@gmail.com>
2023-10-12 21:41:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "../node_modules/react-bootstrap/dist/react-bootstrap";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Login from "./pages/Login";
+import { Route, Switch } from "react-router-dom";
+import { Fragment, useEffect } from "react";
+import Welcome from "./pages/Welcome";
+import ComposeMail from "./pages/ComposeMail";
+import Mailbox from "./pages/Mailbox";
+import SentMail from "./pages/SentMail";
+import { useDispatch } from "react-redux";
+import { fetchRecievedMail, fetchSentMail } from "./store/compose-actions";
+import Inbox from "./pages/Inbox";
+import MessageBox from "./pages/MessageBox";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    setInterval(() => {
+      dispatch(fetchSentMail());
+      dispatch(fetchRecievedMail());
+    }, 2000);
+  }, [dispatch]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Switch>
+        <Route path="/login" exact>
+          <Login />
+        </Route>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/mailbox">
+          <Mailbox />
+        </Route>
+        <Route path="/compose">
+          <Mailbox />
+          <ComposeMail />
+        </Route>
+        <Route path="/sentmail">
+          <Mailbox />
+          <SentMail />
+        </Route>
+        <Route path="/inbox" exact>
+          <Mailbox />
+          <Inbox />
+        </Route>
+        <Route path="/inbox/:email">
+          <Mailbox />
+          <MessageBox />
+        </Route>
+      </Switch>
+    </Fragment>
   );
 }
 
